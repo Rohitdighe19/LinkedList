92762c292bf73ae72a711021d51047c20c66cba2
rohitdighe19 <rohitdighe47@gmail.com>
2021-07-26 11:57:41 +0530

    Added to solution of use-case-3

--- Runner3.java
+++ Runner3.java
@@ -1,9 +1,9 @@
-public class Runner2 {
+public class Runner3 {
     public static void main(String[] args) {
         LinkedList<Integer> numberList = new LinkedList<Integer>();
-        numberList.add(70);
-        numberList.add(30);
         numberList.add(56);
+        numberList.add(30);
+        numberList.add(70);
         numberList.show();
     }
 }
